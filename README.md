2d94ae107758e2afc89eadb14383176469392580
krnch <93.karan@gmail.com>
2017-04-06 16:55:46 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # csgy9223bigdata
 Project for Big Data Spring 2017
+
+Analysing the NYPD Crime data 
+
+Available at -https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i
---
+++ summary/offense_description_4.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    lines = sc.textFile('crime.csv')
+    header=lines.first()
+    lines=lines.filter(lambda row : row !=header)
+    lines= lines.mapPartitions( lambda x:reader(x))
+    counts=lines.map(lambda x:(str(x[7]),1))
+    counts=counts.reduceByKey( lambda a,b :a+b)
+    count1=counts.map(lambda x:(x[1],x[0]))
+    count1=count1.sortByKey(False)
+
+
+    df=count1.map(lambda r: str(r[1])+"\t"+str(r[0]))
+    df.saveAsTextFile("taskwc4.out")
+
+
+

fc35520ee9d03d657fccc54b43d8287ff53fa858
Rohul Amin <rohul@nyu.edu>
2017-04-16 21:27:31 -0400

    Crimes_Completed_Attempted and PD_Desc Columns

---
+++ column_details/crime_completed_desc.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import sys
+from operator import add
+from csv import reader
+import datetime
+import re
+from pyspark import SparkContext
+
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    lines = sc.textFile("crimes.csv")
+    header = lines.first()
+    #lines = lines.take(500)
+    #lines=sc.parallelize(lines)
+    lines = lines.filter(lambda row: row != header)
+    lines = lines.mapPartitions(lambda x: reader(x))
+    counts = lines.map(lambda x: (x[10]))
+    counts = counts.map(lambda x: (x, 1))
+    counts = counts.map(lambda x: (x[0], pd_check_type(x[0]), pd_check_semantic(x[0]), pd_check_null(x[0])))
+
+    def pd_check_null(pdstring):
+        if (pdstring == ""):
+            return 'NULL'
+        elif (pdstring=='COMPLETED' or pdstring=='ATTEMPTED'):
+            return 'VALID'
+        else:
+            return 'INVALID'
+
+
+
+    def pd_check_semantic(pdstring):
+        counted=0
+        if (pdstring == ''):
+            return ''
+        elif (pdstring=='COMPLETED' or pdstring=='ATTEMPTED'):
+            return 'CRIME_ATTEMPTED_COMPLETED'
+        else:
+            return 'JUNK'
+
+
+    def pd_check_type(pdstring):
+        if isinstance(pdstring, str):
+            return 'TEXT'
+        elif isinstance(pdstring, int):
+            return 'NUMBER'
+        else:
+            return ""
+
+
+    counts = counts.map(lambda x: "%s\t%s\t%s\t%s" % (x[0], x[1], x[2], x[3]))
+
+    counts.saveAsTextFile("pd_attempt_descriptions.out")

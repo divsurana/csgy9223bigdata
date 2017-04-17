860cbf20a49e25447bf3d86f971ca00268f8d20c
krnch <93.karan@gmail.com>
2017-04-17 09:41:20 -0400

    update crime analysis

--- analysis/crime_analysis.py
+++ analysis/crime_analysis.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 15, 6
 
-
 #chunksize = 5
 data = pd.read_csv('/home/karan/crime.csv', dtype={"HADEVELOPT": str ,"PREM_TYP_DESC":str,"PARKS_NM":str})
 #DtypeWarning: Columns (17) have mixed types. Specify dtype option on import or set low_memory=False.
@@ -14,14 +13,33 @@ data = pd.read_csv('/home/karan/crime.csv', dtype={"HADEVELOPT": str ,"PREM_TYP_
 print data.head()
 print '\n Data Types:'
 print data.dtypes
+print data.count()
 
+#plot count of all columns
+data.count().plot.bar()
+
+#plot date vs no of crime occurences on that date
 dfreport=pd.DataFrame(data , columns=['RPT_DT'])
-print dfreport.head()
 dfreport['RPT_DT'] = pd.to_datetime(dfreport['RPT_DT'])
-
 dfreport['freq']=dfreport.groupby('RPT_DT')['RPT_DT'].transform('count')
 
 df1=dfreport.drop_duplicates(['RPT_DT'])
-print df1.head()
 df1.plot(['RPT_DT'],['freq'])
+
+#plot time vs no of complaints made at that time
+df1report=pd.DataFrame(data , columns=['CMPLNT_FR_TM'])
+df1report['CMPLNT_FR_TM'] = pd.to_datetime(df1report['CMPLNT_FR_TM'])
+df1report['freq']=df1report.groupby('CMPLNT_FR_TM')['CMPLNT_FR_TM'].transform('count')
+
+df2=df1report.drop_duplicates(['CMPLNT_FR_TM'])
+df2.plot(['CMPLNT_FR_TM'],['freq'])
+
+#plot boroughs vs the no of crime occurances in each borough
+boro = data.groupby('BORO_NM')
+boro.size().plot.bar()
+
 plt.show()
+
+
+
+

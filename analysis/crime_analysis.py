684206624d1d01c4d18f151d9424a6e1ce32bdd2
krnch <93.karan@gmail.com>
2017-04-16 23:06:03 -0400

    update on timeseries

--- analysis/crime_analysis.py
+++ analysis/crime_analysis.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
-
+from datetime import datetime
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 15, 6
 
 
 #chunksize = 5
-data = pd.read_csv('/home/karan/crime.csv', dtype={"HADEVELOPT": str ,"PREM_TYP_DESC":str,"PARKS_NM":str})
-
+data = pd.read_csv('/home/karan/crimenew.csv', dtype={"HADEVELOPT": str ,"PREM_TYP_DESC":str,"PARKS_NM":str})
 #DtypeWarning: Columns (17) have mixed types. Specify dtype option on import or set low_memory=False.
 #full_data = pd.concat(data, ignore_index=True)
 
 print data.head()
 print '\n Data Types:'
 print data.dtypes
+
+dfreport=pd.DataFrame(data , columns=['RPT_DT'])
+print dfreport.head()
+dfreport['RPT_DT'] = pd.to_datetime(dfreport['RPT_DT'])
+
+dfreport['freq']=dfreport.groupby('RPT_DT')['RPT_DT'].transform('count')
+
+df1=dfreport.drop_duplicates(['RPT_DT'])
+print df1.head()
+df1.plot(['RPT_DT'],['freq'])
+plt.show()
